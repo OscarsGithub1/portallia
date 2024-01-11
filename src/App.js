1b5b242ebe1a2e3919721a417a804c8f223545a1
Oscar Enbacka <enbacka.oscar@gmail.com>
2024-01-11 13:14:20 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
 import PrivateRoute from './components/PrivateRoute';
-
+import CompanyList from './components/CompanyList'; // Import the CompanyList component
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -26,10 +26,13 @@ function App() {
             <Login onLogin={handleLogin} />
           </Route>
           <PrivateRoute path="/home" component={Home} isAuthenticated={isLoggedIn} />
-          <Redirect from="/" exact to={isLoggedIn ? "/home" : "/login"} />
           
+          {/* Add a new route for the CompanyList component */}
+          <PrivateRoute path="/companies" component={CompanyList} isAuthenticated={isLoggedIn} />
+          
+          {/* Redirect from "/" exact to "/home" or "/login" based on user's login status */}
+          <Redirect from="/" exact to={isLoggedIn ? "/home" : "/login"} />
         </Switch>
-        
       </div>
     </Router>
   );
