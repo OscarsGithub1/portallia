8c827bc80f692a68faceb4128263c36f32592a57
Simon <simon_bklina@hotmail.com>
2024-04-04 13:51:52 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,23 @@ import { FiLogOut } from 'react-icons/fi';
 import { Dropdown } from 'react-bootstrap';
 import Avatar from '@mui/material/Avatar';
 import Paper from '@mui/material/Paper';
+import Popover from '@mui/material/Popover';
+import IconButton from '@mui/material/IconButton';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
@@ -41,14 +55,42 @@ const Navbar = () => {
                 Förfrågningar
               </a>
               <a className="nav-link border-right" href="/deals" style={{ fontSize: '1.4rem', marginLeft: '10px' }}>
-                Avtal
+                Uppdrag
               </a>
               <a className="nav-link border-right" href="/document" style={{ fontSize: '1.4rem', marginLeft: '10px' }}>
                 TestförOscar
               </a>
             </div>
 
-            <div style={{ marginRight: '20px' }} className="navbar-nav ms-auto">
+            <div style={{ marginRight: '20px', display: 'flex', alignItems: 'center' }} className="navbar-nav ms-auto">
+              <span>Guide</span>
+              <IconButton
+                aria-label="information"
+                aria-describedby="information-popover"
+                onClick={handlePopoverOpen}
+                style={{ marginRight: '10px' }}
+              >
+                <InfoOutlinedIcon />
+              </IconButton>
+              <Popover
+                id="information-popover"
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handlePopoverClose}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+              >
+                <div style={{ padding: '20px' }}>
+                  <h4>Information</h4>
+                  <p>This is some information you want to display.</p>
+                </div>
+              </Popover>
               <Dropdown>
                 <Dropdown.Toggle onClick={toggleDropdown} className="dropdown-toggle">
                   <Avatar src="/broken-image.jpg" />
